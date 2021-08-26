6dda9c289a53bc5603fcd8ae201139b701a8ea13
Nilomiranda <nilomiranda3@gmail.com>
2021-08-25 21:38:36 -0300

    removed useless code

--- packages/server/src/users/models/user.model.ts
+++ packages/server/src/users/models/user.model.ts
@@ -1,14 +1,9 @@
 import {
   Field,
-  Int,
   ObjectType,
-  EnumOptions,
   registerEnumType,
-  FieldMiddleware,
-  MiddlewareContext, NextFn
 } from "@nestjs/graphql";
 import {IsEmail, IsEnum, IsNotEmpty, IsString, MinLength} from "class-validator";
-import * as bcrypt from 'bcrypt'
 
 export enum UserStatus {
   ACTIVE = 'ACTIVE',
@@ -19,14 +14,6 @@ registerEnumType(UserStatus, {
 });
 
 export const PASSWORD_MIN_CHARACTERS = 8
-
-const hashPasswordMiddleware: FieldMiddleware = async (context: MiddlewareContext, next: NextFn) => {
-  const password = await next()
-  console.log('password', password)
-  const hashedPassword = await bcrypt.hash(password, 8)
-  console.log('hashedPassword', hashedPassword)
-  return hashedPassword;
-}
 
 @ObjectType()
 export class User {
@@ -46,7 +33,6 @@ export class User {
   @IsString({ always: true })
   @IsNotEmpty({ always: true })
   @MinLength(PASSWORD_MIN_CHARACTERS, { message: `Passwords must have at least ${PASSWORD_MIN_CHARACTERS} characters` })
-  @Field(type => String, { nullable: true, middleware: [hashPasswordMiddleware] })
   password: string;
 
   @IsString({ always: true })
