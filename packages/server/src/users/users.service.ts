9ab14e01b29351ba2a8e25b3303a42faf16f7177
Nilomiranda <nilomiranda3@gmail.com>
2021-08-25 21:39:19 -0300

    sending email after creating account (account confirmation email)

--- packages/server/src/users/users.service.ts
+++ packages/server/src/users/users.service.ts
@@ -2,13 +2,23 @@ import {User, UserStatus} from "./models/user.model";
 import {getPrismaClient} from "../main";
 import {CreateUserInput} from "./dto/createUserInput";
 import {generateRandomCode} from "../_helpers/generateRandomCode";
+import * as bcrypt from "bcrypt";
+import {sendEmailWithTemplate} from "../services/mailjet";
+
+const ACCOUNT_CONFIRMATION_TEMPLATE_NUMBER = 3125538
 
 export class UsersService {
   async create(args: CreateUserInput) {
     const prisma = getPrismaClient()
-    return prisma.user.create({
-      data: { ...args, status: UserStatus.UNCONFIRMED_ACCOUNT, confirmationCode: generateRandomCode() },
+    const user = await prisma.user.create({
+      data: { ...args, password: await this.hashPassword(args?.password),  status: UserStatus.UNCONFIRMED_ACCOUNT, confirmationCode: generateRandomCode() },
     })
+
+    sendEmailWithTemplate(ACCOUNT_CONFIRMATION_TEMPLATE_NUMBER, [{ Email: user?.email, Name: user?.name }], 'DFinanz account confirmation', { email: user?.email, name: user?.name, confirmationCode: user?.confirmationCode }).catch(err => {
+      console.error('Error sending account confirmation error', err)
+    })
+
+    return user
   }
 
   async getOne(id: string) {
@@ -20,5 +30,9 @@ export class UsersService {
     })
 
     return user
+  }
+
+  hashPassword(password: string): Promise<string> {
+    return bcrypt.hash(password, 8)
   }
 }
