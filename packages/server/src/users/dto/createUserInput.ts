da153e01fb23bdbb5492c264cb13eec953aba21c
Nilomiranda <nilomiranda3@gmail.com>
2021-08-25 22:15:51 -0300

    remove unused code and imports

--- packages/server/src/users/dto/createUserInput.ts
+++ packages/server/src/users/dto/createUserInput.ts
@@ -1,15 +1,6 @@
-import {Field, FieldMiddleware, InputType, MiddlewareContext, NextFn} from "@nestjs/graphql";
+import {Field, InputType} from "@nestjs/graphql";
 import {IsEmail, IsNotEmpty, IsString, MinLength} from "class-validator";
 import {PASSWORD_MIN_CHARACTERS} from "../models/user.model";
-import * as bcrypt from "bcrypt";
-
-const hashPasswordMiddleware: FieldMiddleware = async (context: MiddlewareContext, next: NextFn) => {
-  const password = await next()
-  console.log('password', password)
-  const hashedPassword = await bcrypt.hash(password, 8)
-  console.log('hashedPassword', hashedPassword)
-  return hashedPassword;
-}
 
 @InputType()
 export class CreateUserInput {
@@ -26,6 +17,6 @@ export class CreateUserInput {
   @IsString({ always: true })
   @IsNotEmpty({ always: true })
   @MinLength(PASSWORD_MIN_CHARACTERS, { message: `Passwords must have at least ${PASSWORD_MIN_CHARACTERS} characters` })
-  @Field(type => String, { nullable: true, middleware: [hashPasswordMiddleware] })
+  @Field(type => String, { nullable: true })
   password: string;
 }
