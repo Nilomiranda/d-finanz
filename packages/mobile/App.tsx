fd7d1a648b0bc7c9b3a70c277deec5235f9d7a70
Nilomiranda <nilomiranda3@gmail.com>
2021-08-26 22:01:36 -0300

    finished sertting up react navigation dependencies

--- packages/mobile/App.tsx
+++ packages/mobile/App.tsx
@@ -1,22 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import {NavigationContainer} from "@react-navigation/native";
+import MainNavigator from "./src/navigators/MainNavigator";
 
 export default function App() {
   return (
     <NavigationContainer>
-      <View style={styles.container}>
-        <Text>Open up App.tsx to start working on your app!</Text>
-      </View>
+      <MainNavigator />
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
