d5d6e8293da2469330435b6967013d2a8568d361
Nilomiranda <nilomiranda3@gmail.com>
2021-08-27 22:05:23 -0300

    created input component to support label and error message

---
+++ packages/mobile/src/components/forms/Input.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import {Input as NativeInput, IInputProps, Box, Text} from 'native-base'
+
+interface InputProps extends IInputProps {
+  label?: string
+  errorMessage?: string
+}
+
+const Input = (props: InputProps) => {
+  const { label, errorMessage } = props
+
+  return (
+    <Box>
+      { label ? <Text fontWeight={"bold"} marginBottom={1.5} color={errorMessage ? "red.500" : "black"}>{label}</Text> : null }
+
+      <NativeInput {...props} variant={"outline"} />
+
+      { errorMessage ? <Text fontSize={"sm"} marginTop={1.5} color={"red.500"}>{errorMessage}</Text> : null }
+    </Box>
+  )
+}
+
+export default Input
