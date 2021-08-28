ff7bce7d85ec757f42ab5051ca1ebbb31dccacdb
Nilomiranda <nilomiranda3@gmail.com>
2021-08-27 22:39:42 -0300

    added account confirmation in routing

--- packages/mobile/src/navigators/MainNavigator.tsx
+++ packages/mobile/src/navigators/MainNavigator.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import SignInScreen from "../auth/screens/SignInScreen";
 import SignUpScreen from "../auth/screens/SignUpScreen";
+import AccountConfirmationScreen from "../auth/screens/AccountConfirmationScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -10,6 +11,7 @@ function MainNavigator() {
     <Stack.Navigator initialRouteName="SignIn">
       <Stack.Screen name="SignIn" component={SignInScreen} />
       <Stack.Screen name="SignUp" component={SignUpScreen} />
+      <Stack.Screen name="AccountConfirmation" component={AccountConfirmationScreen} />
     </Stack.Navigator>
   )
 }
