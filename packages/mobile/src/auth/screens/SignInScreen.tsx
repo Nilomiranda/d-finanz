74356292e16e80232eb486946678c0a125189e9d
Nilomiranda <nilomiranda3@gmail.com>
2021-08-27 22:05:55 -0300

    added some components to test

--- packages/mobile/src/auth/screens/SignInScreen.tsx
+++ packages/mobile/src/auth/screens/SignInScreen.tsx
@@ -1,13 +1,31 @@
 import React from 'react'
-import {Text} from "react-native";
-import {Box, Button} from "native-base";
+import {Box, Button, Checkbox, Radio} from "native-base";
+import Input from '../../components/forms/Input'
 
 const SignInScreen = () => {
   return (
     <Box>
       Sign In Screen
 
-      <Button>Sign In</Button>
+      <Button onPress={() => null} variant={"solid"}>Test</Button>
+      <Button onPress={() => null} variant={"outline"}>Test</Button>
+      <Button onPress={() => null} variant={"link"}>Test</Button>
+
+      <Button onPress={() => null} variant={"solid"} size={"lg"} colorScheme={"red"}>Test</Button>
+      <Button onPress={() => null} variant={"outline"} size={"lg"} colorScheme={"red"}>Test</Button>
+      <Button onPress={() => null} variant={"link"} size={"lg"} colorScheme={"red"}>Test</Button>
+
+      <Input label="Email" />
+      <Input label="Password" type={"password"} />
+      <Input label="Email" isInvalid={true} errorMessage={"Invalid email"} />
+
+      <Checkbox value={"Yes"}>Yes</Checkbox>
+      <Checkbox value={"No"}>No</Checkbox>
+
+      <Radio.Group name={"test"}>
+        <Radio value={"No"} my={1}>No</Radio>
+        <Radio value={"Yes"} my={1}>Yes</Radio>
+      </Radio.Group>
     </Box>
   )
 }
