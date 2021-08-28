9637390035d11621257892c57bfc2da593582720
Nilomiranda <nilomiranda3@gmail.com>
2021-08-27 22:26:46 -0300

    sign up form

--- packages/mobile/src/auth/screens/SignUpScreen.tsx
+++ packages/mobile/src/auth/screens/SignUpScreen.tsx
@@ -1,9 +1,40 @@
 import React from 'react'
-import {Text} from "react-native";
+import {Box, Button, Heading, HStack, Link, VStack, Text} from "native-base";
+import Input from "../../components/forms/Input";
 
-const SignUpScreen = () => {
+const SignUpScreen = ({ navigation }) => {
+  const handleNavigateToSignIn = () => {
+    navigation?.navigate('SignIn')
+  }
+
   return (
-    <Text>Sign Up Screen</Text>
+    <VStack paddingX={3} justifyContent={"center"} height={"100%"}>
+      <Heading textAlign={"center"} mb={10}>Sign up</Heading>
+
+      <Box mb={5}>
+        <Input label={"Email"} placeholder={"email@domain.com"} />
+      </Box>
+
+      <Box mb={5}>
+        <Input label={"Name"} placeholder={"Robert"} />
+      </Box>
+
+      <Box mb={5}>
+        <Input label={"Password"} type={"password"} placeholder={"********"} />
+      </Box>
+
+      <Box mb={5}>
+        <Input label={"Password confirmation"} type={"password"} placeholder={"********"} />
+      </Box>
+
+      <Box mb={5}>
+        <Button size={"lg"}>Create account</Button>
+      </Box>
+
+      <HStack>
+        <Text>Have an account? </Text><Link _text={{ textDecoration: 'underline' }} onPress={handleNavigateToSignIn}>Sign in</Link>
+      </HStack>
+    </VStack>
   )
 }
 
