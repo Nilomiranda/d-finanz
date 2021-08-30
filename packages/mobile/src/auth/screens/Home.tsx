00c382e4f9f1abf7d74e5a5af3792cab2282ef63
Nilomiranda <nilomiranda3@gmail.com>
2021-08-30 20:40:03 -0300

    added logged user screen (testing)

---
+++ packages/mobile/src/auth/screens/Home.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {Text} from "native-base";
+
+const Home = () => {
+  return (
+    <Text>Hello from home</Text>
+  )
+}
+
+export default Home
--- packages/mobile/src/navigators/MainNavigator.tsx
+++ packages/mobile/src/navigators/MainNavigator.tsx
@@ -3,6 +3,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import SignInScreen from "../auth/screens/SignInScreen";
 import SignUpScreen from "../auth/screens/SignUpScreen";
 import AccountConfirmationScreen from "../auth/screens/AccountConfirmationScreen";
+import Home from "../auth/screens/Home";
 
 const Stack = createNativeStackNavigator();
 
@@ -12,6 +13,7 @@ function MainNavigator() {
       <Stack.Screen name="SignIn" component={SignInScreen} />
       <Stack.Screen name="SignUp" component={SignUpScreen} />
       <Stack.Screen name="AccountConfirmation" component={AccountConfirmationScreen} />
+      <Stack.Screen name="Home" component={Home} />
     </Stack.Navigator>
   )
 }
