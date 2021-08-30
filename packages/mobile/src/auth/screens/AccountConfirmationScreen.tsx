f4e40396fd96b5b74719d8432e0d065a356d36c5
Nilomiranda <nilomiranda3@gmail.com>
2021-08-30 20:13:15 -0300

    reading email coming from recent signup

--- packages/mobile/src/auth/screens/AccountConfirmationScreen.tsx
+++ packages/mobile/src/auth/screens/AccountConfirmationScreen.tsx
@@ -1,18 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Box, Button, Heading, HStack, Link, VStack, Text} from "native-base";
 import Input from "../../components/forms/Input";
 
-const AccountConfirmationScreen = () => {
+const AccountConfirmationScreen = ({ route, navigation }) => {
+  const [email, setEmail] = useState<string>(route?.params?.email)
+  const [code, setCode] = useState('')
+
   return (
     <VStack paddingX={3} justifyContent={"center"} height={"100%"}>
       <Heading textAlign={"center"} mb={10}>Confirm your account</Heading>
 
       <Box mb={5}>
-        <Input label={"Email"} placeholder={"email@domain.com"} />
+        <Input label={"Email"} placeholder={"email@domain.com"} value={email} onChangeText={(value) => setEmail(value)} />
       </Box>
 
       <Box mb={5}>
-        <Input label={"Code"} placeholder={"0000"} />
+        <Input label={"Code"} placeholder={"0000"} value={code} onChangeText={(value) => setCode(value)} />
       </Box>
 
       <Box mb={5}>
