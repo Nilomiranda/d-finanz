a157583e3154122041088f48124c03e4609c6246
Nilomiranda <nilomiranda3@gmail.com>
2021-08-30 20:13:01 -0300

    implemented mutation call to create account

--- packages/mobile/src/auth/screens/SignUpScreen.tsx
+++ packages/mobile/src/auth/screens/SignUpScreen.tsx
@@ -1,8 +1,26 @@
-import React from 'react'
-import {Box, Button, Heading, HStack, Link, VStack, Text} from "native-base";
+import React, {useEffect, useState} from 'react'
+import {Box, Button, Heading, HStack, Link, VStack, Text, useToast} from "native-base";
 import Input from "../../components/forms/Input";
+import {useMutation} from "urql";
+
+const SignUpMutation = `
+    mutation($name: String!, $email: String!, $password: String!) {
+      createUser(input: { name: $name, email: $email, password: $password }) {
+        name
+        email
+      }
+    }
+  `
 
 const SignUpScreen = ({ navigation }) => {
+  const toast = useToast()
+  const [signUpResult, signUp] = useMutation(SignUpMutation)
+
+  const [name, setName] = useState<string>()
+  const [email, setEmail] = useState<string>()
+  const [password, setPassword] = useState<string>()
+  const [creatingAccount, setCreatingAccount] = useState(false)
+
   const handleNavigateToSignIn = () => {
     navigation?.navigate('SignIn')
   }
@@ -11,20 +29,55 @@ const SignUpScreen = ({ navigation }) => {
     navigation?.navigate('AccountConfirmation')
   }
 
+  const handleAccountCreated = () => {
+    navigation?.navigate('AccountConfirmation', { email })
+  }
+
+  const handleSignUp = async () => {
+    try {
+      setCreatingAccount(true)
+      await signUp({
+        name,
+        email,
+        password
+      })
+      handleAccountCreated()
+      toast.show({
+        description: 'Account created! Check your email for a confirmation code.',
+        status: 'info',
+        isClosable: true,
+        duration: 5000,
+      })
+    } catch (err) {
+      toast.show({
+        description: 'Error trying to create your account. Please try again in few moments',
+        status: 'error',
+        isClosable: true,
+        duration: 5000,
+      })
+    } finally {
+      setCreatingAccount(false)
+    }
+  }
+
+  useEffect(() => {
+    console.log('signUpResult', signUpResult)
+  }, [signUpResult])
+
   return (
     <VStack paddingX={3} justifyContent={"center"} height={"100%"}>
       <Heading textAlign={"center"} mb={10}>Sign up</Heading>
 
       <Box mb={5}>
-        <Input label={"Email"} placeholder={"email@domain.com"} />
+        <Input label={"Email"} placeholder={"email@domain.com"} autoCapitalize={"none"} value={email} onChangeText={(value) => setEmail(value)} />
       </Box>
 
       <Box mb={5}>
-        <Input label={"Name"} placeholder={"Robert"} />
+        <Input label={"Name"} placeholder={"Robert"} value={name} onChangeText={(value) => setName(value)} />
       </Box>
 
       <Box mb={5}>
-        <Input label={"Password"} type={"password"} placeholder={"********"} />
+        <Input label={"Password"} type={"password"} placeholder={"********"} value={password} onChangeText={(value) => setPassword(value)} />
       </Box>
 
       <Box mb={5}>
@@ -32,7 +85,7 @@ const SignUpScreen = ({ navigation }) => {
       </Box>
 
       <Box mb={5}>
-        <Button size={"lg"}>Create account</Button>
+        <Button size={"lg"} onPress={handleSignUp} isLoading={creatingAccount}>Create account</Button>
       </Box>
 
       <HStack>
