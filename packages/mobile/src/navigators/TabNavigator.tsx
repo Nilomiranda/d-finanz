3d6e0f0c6d5aab6d472aefe3b050e044f0881bcf
Nilomiranda <nilomiranda3@gmail.com>
2021-09-02 20:06:47 -0300

    added icons to tabs and created profile screen

--- packages/mobile/src/navigators/TabNavigator.tsx
+++ packages/mobile/src/navigators/TabNavigator.tsx
@@ -1,21 +1,40 @@
 import React from 'react'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { BottomTabNavigationOptions, createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import AddIncomeScreen from '../finances/screens/AddIncomeScreen';
 import AddExpenseScreen from '../finances/screens/AddExpenseScreen';
+import MaterialIcon from 'react-native-vector-icons/MaterialIcons'
+import { RouteProp } from '@react-navigation/core';
+import ProfileScreen from '../profile/screens/ProfileScreen';
 
 export type BottomTabParamsList = {
   AddIncome: undefined
   AddExpense: undefined
+  Profile: undefined
 }
 
 
 const Tab = createBottomTabNavigator<BottomTabParamsList>()
 
+const setTabBarScreenOptions = ({ route }: { route: RouteProp<BottomTabParamsList> }): BottomTabNavigationOptions => ({
+  tabBarIcon: ({ focused }) => {
+    const routeNameToIconName: Record<string, string> = {
+      AddIncome: 'attach-money',
+      AddExpense: 'money-off',
+      Profile: 'person'
+    }
+
+    return <MaterialIcon name={routeNameToIconName[route?.name]} color={focused ? "black" : "lightgray"} size={30} />
+  },
+  tabBarActiveTintColor: 'black',
+  tabBarInactiveTintColor: 'lightgray',
+})
+
 const MainTabNavigator = () => {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator screenOptions={setTabBarScreenOptions}>
       <Tab.Screen name="AddIncome" component={AddIncomeScreen} />
       <Tab.Screen name="AddExpense" component={AddExpenseScreen} />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
     </Tab.Navigator>
   )
 }
---
+++ packages/mobile/src/profile/screens/ProfileScreen.tsx
@@ -0,0 +1,10 @@
+import { Text } from 'native-base'
+import React from 'react'
+
+const ProfileScreen = () => {
+  return (
+    <Text>ProfileScreen</Text>
+  )
+}
+
+export default ProfileScreen
