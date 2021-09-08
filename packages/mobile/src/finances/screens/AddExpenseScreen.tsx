9e78e4146f36359a4b44d1bdaeb80cea18328dcd
Nilomiranda <nilomiranda3@gmail.com>
2021-09-08 20:50:41 -0300

    fixed fab button display

--- packages/mobile/src/finances/screens/AddExpenseScreen.tsx
+++ packages/mobile/src/finances/screens/AddExpenseScreen.tsx
@@ -1,6 +1,7 @@
 import { Text, Fab, VStack } from 'native-base'
 import MaterialIcon from 'react-native-vector-icons/MaterialIcons'
 import React from 'react'
+import { useIsFocused } from '@react-navigation/native';
 import { NativeStackScreenProps } from '@react-navigation/native-stack'
 import { ExpensesStackParamsList } from '../../navigators/ExpensesNavigator'
 import { FinancialRecordType } from '../../interfaces/financialRecord'
@@ -8,17 +9,23 @@ import { FinancialRecordType } from '../../interfaces/financialRecord'
 type ExpensesScreenProps = NativeStackScreenProps<ExpensesStackParamsList, 'List'>
 
 const ExpensesScreen = ({ navigation }: ExpensesScreenProps) => {
+  const isFocused = useIsFocused()
+
   return (
     <VStack>
       <Text>Expenses screen</Text>
 
-      <Fab
-        placement="bottom-right"
-        size="sm"
-        icon={<MaterialIcon name="add" size={24} />}
-        mb={20}
-        onPress={() => navigation?.navigate('Form', { type: FinancialRecordType.EXPENSE })}
-      />
+      {
+        isFocused ?
+        (
+          <Fab
+            size="sm"
+            icon={<MaterialIcon name="add" size={24} />}
+            mb={20}
+            onPress={() => navigation?.navigate('Form', { type: FinancialRecordType.EXPENSE })}
+          />
+        ) : null
+      }
     </VStack>
   )
 }
